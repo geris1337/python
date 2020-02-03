12d9b3df81f4b94e7b421c61fe69823fa75d4dc3
geris1337 <50952907+geris1337@users.noreply.github.com>
2020-02-03 12:46:51 +0200

    Add files via upload

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 sudo apt update  
-sudo apt install firefox firefox-geckodriver python3 python3-pip -y
+sudo apt install wget python3 python3-pip firefox-esr -y
 pip3 install -U pytest selenium
+wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-linux64.tar.gz
+tar -xvzf geckodriver-v0.26.0-linux64.tar.gz
+rm geckodriver-v0.26.0-linux64.tar.gz
+chmod +x geckodriver
+sudo cp geckodriver /usr/local/bin/
