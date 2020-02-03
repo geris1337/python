5fa2e387e30ce073ac559de348b7893d9dfddd5f
geris1337 <50952907+geris1337@users.noreply.github.com>
2020-02-03 13:03:22 +0200

    Update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 sudo apt update  
-sudo apt install wget python3 python3-pip firefox-esr -y
+sudo apt install wget python3 python3-pip -y
+sudo apt install firefox-esr
 sudo apt install firefox -y
 pip3 install -U pytest selenium
 wget https://github.com/mozilla/geckodriver/releases/download/v0.26.0/geckodriver-v0.26.0-linux64.tar.gz
